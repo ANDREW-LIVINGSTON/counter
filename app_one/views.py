4206df3560c8511f863c8527e332b8d894229950
Andrew Livingston <andrewlivingston@MacBook-Pro.attlocal.net>
2021-03-17 16:14:41 -0500

    first commit

---
+++ app_one/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+
+def index(request):
+    if 'counter' not in request.session:
+        print('key exists!')
+        request.session ['counter'] = 0
+    request.session ['counter'] += 1
+    return render(request, 'index.html')
+
+def destroy(request):
+    del request.session['counter']
+    return redirect('/')
+
+def double(request):
+    request.session['counter'] += 2
+    return render(request, 'index.html')
